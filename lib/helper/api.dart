1d482e6c5a8fd4789bf62d514f1ea50291cf2ae5
m9stafa05 <m9stafa05@gmail.com>
2025-03-29 22:24:39 +0200

    add post and updata Requset

--- lib/helper/api.dart
+++ lib/helper/api.dart
@@ -1,13 +1,68 @@
 import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
 
 class Api {
-  Future<dynamic> get({required String url}) async {
+  Future<dynamic> get({
+    required String url,
+    @required String? token,
+  }) async {
+    Map<String, String> headers = {};
+    if (token != null) {
+      headers.addAll({'Authorization': 'Bearer $token'});
+    }
     Response response = await Dio().get(url);
     if (response.statusCode == 200) {
       return response.data;
     } else {
       throw Exception(
         "The request failed with status: ${response.statusCode}",
+      );
+    }
+  }
+
+  Future<dynamic> post({
+    required String url,
+    @required dynamic data,
+    @required String? token,
+  }) async {
+    Map<String, String> headers = {};
+    if (token != null) {
+      headers.addAll({'Authorization': 'Bearer $token'});
+    }
+    Response response = await Dio().post(
+      url,
+      data: data,
+      options: Options(headers: headers),
+    );
+    if (response.statusCode == 200) {
+      return response.data;
+    } else {
+      throw Exception(
+        "The request failed with status: ${response.statusCode} with body ${response.data}",
+      );
+    }
+  }
+
+  Future<dynamic> put({
+    required String url,
+    @required dynamic data,
+    @required String? token,
+  }) async {
+    Map<String, String> headers = {};
+    headers.addAll({'Content-Type': 'application/json'});
+    if (token != null) {
+      headers.addAll({'Authorization': 'Bearer $token'});
+    }
+    Response response = await Dio().post(
+      url,
+      data: data,
+      options: Options(headers: headers),
+    );
+    if (response.statusCode == 200) {
+      return response.data;
+    } else {
+      throw Exception(
+        "The request failed with status: ${response.statusCode} with body ${response.data}",
       );
     }
   }
