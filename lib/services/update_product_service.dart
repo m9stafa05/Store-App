708be67c210011c610924b596f8ce7f79c863b6d
m9stafa05 <m9stafa05@gmail.com>
2025-04-02 04:59:11 +0200

    Modifiy Edits & Solve Confg

--- lib/services/update_product_service.dart
+++ lib/services/update_product_service.dart
@@ -1,18 +1,21 @@
+import 'package:flutter/cupertino.dart';
 import 'package:store_app/constants.dart';
 import 'package:store_app/helper/api.dart';
 import 'package:store_app/models/product_model.dart';
 
 class UpdateProductService {
-   Future<dynamic> updateProduct({
+  Future<dynamic> updateProduct({
     required String title,
     required String price,
     required String description,
     required String image,
+    required int id,
     required String category,
   }) async {
+    debugPrint('\n id: $id');
     // ignore: missing_required_param
-    Map<String, dynamic> jsonData = await Api().post(
-      url: baseUrl,
+    Map<String, dynamic> jsonData = await Api().put(
+      url: '$baseUrl/$id',
       data: {
         "title": title,
         "price": price,
