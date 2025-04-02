a25b4b692c12e09e11a6b8a28ca7877918d76d50
m9stafa05 <m9stafa05@gmail.com>
2025-04-02 04:49:47 +0200

    Add Cutom Text Field widget

---
+++ lib/widgets/custam_form_textfield.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+// ignore: must_be_immutable
+class CustamTextField extends StatelessWidget {
+  CustamTextField({
+    super.key,
+    required this.labelText,
+    this.hintText = '',
+    this.onChange,
+    this.isPassword = false,
+    this.textInputType = TextInputType.text,
+  });
+  final String labelText;
+  final String hintText;
+  Function(String)? onChange;
+  final bool isPassword;
+  TextInputType textInputType;
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+      child: SizedBox(
+        width: 400,
+        child: TextField(
+          obscureText: isPassword == true ? true : false,
+          keyboardType: textInputType,
+          onChanged: onChange,
+          style: TextStyle(
+            color: const Color(0xFF000000),
+            fontSize: 15,
+          ),
+          decoration: InputDecoration(
+            hintText: hintText,
+            hintStyle: TextStyle(
+              color: const Color(0xFF949494),
+              fontSize: 10,
+            ),
+            labelText: labelText,
+            labelStyle: TextStyle(
+              color: const Color(0xFF000000),
+              fontSize: 15,
+            ),
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(16),
+              borderSide: BorderSide(color: const Color(0xFF000000)),
+            ),
+            enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(16),
+              borderSide: BorderSide(color: const Color(0xFF000000)),
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10),
+              borderSide: BorderSide(color: Colors.blue),
+            ),
+            errorBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(16),
+              borderSide: BorderSide(color: Colors.red),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
